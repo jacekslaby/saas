daeb7037d6e34a5cc0af9bc14b4b37dff74f2b81
Jacek Slaby <jacek.slaby@interia.pl>
2018-12-06 14:23:03 +0000

    Corrected endless deadlock due to no callback invocation.

--- src/main/java/com/j9soft/saas/alarms/dao/RequestDaoDevMock.java
+++ src/main/java/com/j9soft/saas/alarms/dao/RequestDaoDevMock.java
@@ -4,6 +4,8 @@ import com.j9soft.krepository.v1.commandsmodel.CreateEntityRequestV1;
 import com.j9soft.krepository.v1.commandsmodel.DeleteEntityRequestV1;
 import com.j9soft.krepository.v1.commandsmodel.ResyncAllEndSubdomainRequestV1;
 import com.j9soft.krepository.v1.commandsmodel.ResyncAllStartSubdomainRequestV1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -17,24 +19,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class RequestDaoDevMock implements RequestDao {
 
+    private static final Logger logger = LoggerFactory.getLogger(RequestDaoDevMock.class);
+
     @Override
     public void saveNewRequest(CreateEntityRequestV1 request, RequestDao.Callback callback) {
-
+        logger.info("RequestUuid:{} - saveNewRequest(CreateEntityRequestV1)", request.getUuid());
+        callback.onCompletion(null);
     }
 
     @Override
     public void saveNewRequest(DeleteEntityRequestV1 request, RequestDao.Callback callback) {
-
+        logger.info("RequestUuid:{} - saveNewRequest(DeleteEntityRequestV1)", request.getUuid());
+        callback.onCompletion(null);
     }
 
     @Override
     public void saveNewRequest(ResyncAllStartSubdomainRequestV1 request, RequestDao.Callback callback) {
-
+        logger.info("RequestUuid:{} - saveNewRequest(ResyncAllStartSubdomainRequestV1)", request.getUuid());
+        callback.onCompletion(null);
     }
 
     @Override
     public void saveNewRequest(ResyncAllEndSubdomainRequestV1 request, RequestDao.Callback callback) {
-
+        logger.info("RequestUuid:{} - saveNewRequest(ResyncAllEndSubdomainRequestV1)", request.getUuid());
+        callback.onCompletion(null);
     }
 
 }
