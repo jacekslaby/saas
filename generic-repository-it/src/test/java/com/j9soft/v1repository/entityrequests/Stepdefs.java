e0ae287445b3e601ee9e8a447de62d304c440e8b
Jacek Slaby <jacek.slaby@interia.pl>
2018-12-11 15:32:01 +0000

    Refactor IT scenarios - using parameters in steps.

--- generic-repository-it/src/test/java/com/j9soft/v1repository/entityrequests/Stepdefs.java
+++ generic-repository-it/src/test/java/com/j9soft/v1repository/entityrequests/Stepdefs.java
@@ -14,14 +14,10 @@ import static org.junit.Assert.*;
 public class Stepdefs {
     private List<String> receivedEntities = new ArrayList<>();
 
-    @Given("^Entity SourceAlarm A does not exist$")
-    public void entity_SourceAlarm_A_does_not_exist() throws Exception {
-        // @TODO Write code here that turns the phrase above into concrete actions
-    }
+    @Given("^Entity SourceAlarm \"([^\"]*)\" does not exist$")
+    public void entity_SourceAlarm_does_not_exist(String entityLabel) throws Exception {
+        // @TODO: Write code here that turns the phrase above into concrete actions
 
-    @Given("^Entity SourceAlarm B does not exist$")
-    public void entity_SourceAlarm_B_does_not_exist() throws Exception {
-        // @TODO Write code here that turns the phrase above into concrete actions
     }
 
     @Given("^I subscribe to EntitiesTopic$")
@@ -31,26 +27,16 @@ public class Stepdefs {
         receivedEntities.add("B");
     }
 
-    @When("^I send CreateEntityRequest with SourceAlarm A$")
-    public void i_send_CreateEntityRequest_with_SourceAlarm_A() throws Exception {
+    @When("^I send CreateEntityRequest with SourceAlarm \"([^\"]*)\"$")
+    public void i_send_CreateEntityRequest_with_SourceAlarm_A(String sourceAlarmLabel) throws Exception {
         // @TODO Write code here that turns the phrase above into concrete actions
     }
 
-    @When("^I send CreateEntityRequest with SourceAlarm B$")
-    public void i_send_CreateEntityRequest_with_SourceAlarm_B() throws Exception {
-        // @TODO Write code here that turns the phrase above into concrete actions
-    }
+    @Then("^I should receive Entity SourceAlarm \"([^\"]*)\"$")
+    public void i_should_receive_Entity_SourceAlarm_A(String entityLabel) throws Exception {
 
-    @Then("^I should receive Entity SourceAlarm A$")
-    public void i_should_receive_Entity_SourceAlarm_A() throws Exception {
-
-        assertTrue("SourceAlarm A was not received", receivedEntities.remove("A"));
-    }
-
-    @Then("^I should receive Entity SourceAlarm B$")
-    public void i_should_receive_Entity_SourceAlarm_B() throws Exception {
-
-        assertTrue("SourceAlarm B was not received", receivedEntities.remove("B"));
+        assertTrue(MessageFormat.format("SourceAlarm {0} was not received", entityLabel),
+                receivedEntities.remove(entityLabel));
     }
 
     @Then("^I should not receive any other Entities$")
--- generic-repository-it/src/test/resources/com/j9soft/v1repository/entityrequests/sending_create_requests_produces_new_entities.feature
+++ generic-repository-it/src/test/resources/com/j9soft/v1repository/entityrequests/sending_create_requests_produces_new_entities.feature
@@ -2,21 +2,21 @@ Feature: Sending create requests produces new entities
   Sending CreateEntityRequests results in new Entities being published to the EntitiesTopic.
 
   Scenario: Send 2 CreateEntityRequests and receive 2 Entities
-    Given Entity SourceAlarm A does not exist
-    And Entity SourceAlarm B does not exist
+    Given Entity SourceAlarm "A" does not exist
+    And Entity SourceAlarm "B" does not exist
     And I subscribe to EntitiesTopic
-    When I send CreateEntityRequest with SourceAlarm A
-    And I send CreateEntityRequest with SourceAlarm B
-    Then I should receive Entity SourceAlarm A
-    And I should receive Entity SourceAlarm B
+    When I send CreateEntityRequest with SourceAlarm "A"
+    And I send CreateEntityRequest with SourceAlarm "B"
+    Then I should receive Entity SourceAlarm "A"
+    And I should receive Entity SourceAlarm "B"
     And I should not receive any other Entities
 
   Scenario: Send 2 CreateEntityRequests and have 2 Entities stored in Repository
-    Given Entity SourceAlarm A does not exist
-    And Entity SourceAlarm B does not exist
-    When I send CreateEntityRequest with SourceAlarm A
-    And I send CreateEntityRequest with SourceAlarm B
+    Given Entity SourceAlarm "A" does not exist
+    And Entity SourceAlarm "B" does not exist
+    When I send CreateEntityRequest with SourceAlarm "A"
+    And I send CreateEntityRequest with SourceAlarm "B"
     And I poll all Entities of type SourceAlarm from Repository
-    Then I should receive Entity SourceAlarm A
-    And I should receive Entity SourceAlarm B
+    Then I should receive Entity SourceAlarm "A"
+    And I should receive Entity SourceAlarm "B"
     And I should not receive any other Entities
