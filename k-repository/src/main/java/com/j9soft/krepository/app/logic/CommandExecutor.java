1a4114bbb747bd708b1d90775d25f1d6c0b90b94
Jacek Slaby <jacek.slaby@interia.pl>
2018-12-17 09:59:35 +0000

    Added logic to support commands: CreateEntityRequestV1 and DeleteEntityRquestV1.

--- k-repository/src/main/java/com/j9soft/krepository/app/logic/CommandExecutor.java
+++ k-repository/src/main/java/com/j9soft/krepository/app/logic/CommandExecutor.java
@@ -1,10 +1,14 @@
 package com.j9soft.krepository.app.logic;
 
+import com.j9soft.krepository.v1.commandsmodel.CreateEntityRequestV1;
+import com.j9soft.krepository.v1.commandsmodel.DeleteEntityRequestV1;
 import com.j9soft.krepository.v1.entitiesmodel.EntityV1;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.streams.kstream.ValueJoiner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
 
 /**
  * Logic to build new value for an Entity based on received Request.
@@ -18,16 +22,92 @@ public class CommandExecutor implements ValueJoiner<GenericRecord, GenericRecord
 
     private static final Logger logger = LoggerFactory.getLogger(CommandExecutor.class);
 
+    // Dedicated value returned when unsupported command was received.
+    public static final EntityV1 UKNOWN_ENTITY_TO_BE_IGNORED = EntityV1.newBuilder()
+            .setUuid("dummy")
+            .setEntryDate(System.currentTimeMillis())
+            .setEntityTypeName("dummy")
+            .setEntitySubdomainName("dummy")
+            .setEntityIdInSubdomain("UKNOWN_ENTITY_TO_BE_IGNORED")
+            .build();
+
+    // Dedicated value returned when a create command was received for already existing entity.
+    public static final EntityV1 ALREADY_EXISTING_ENTITY_TO_BE_IGNORED = EntityV1.newBuilder()
+            .setUuid("dummy")
+            .setEntryDate(System.currentTimeMillis())
+            .setEntityTypeName("dummy")
+            .setEntitySubdomainName("dummy")
+            .setEntityIdInSubdomain("ALREADY_EXISTING_ENTITY_TO_BE_IGNORED")
+            .build();
+
+    // Dedicated value returned when a delete command was received for not existing entity.
+    public static final EntityV1 NOT_EXISTING_ENTITY_TO_BE_IGNORED = EntityV1.newBuilder()
+            .setUuid("dummy")
+            .setEntryDate(System.currentTimeMillis())
+            .setEntityTypeName("dummy")
+            .setEntitySubdomainName("dummy")
+            .setEntityIdInSubdomain("NOT_EXISTING_ENTITY_TO_BE_IGNORED")
+            .build();
+
+
     @Override
-    public EntityV1 apply(GenericRecord command, GenericRecord oldEntityValue) {
-        EntityV1 newEntityValue = null;
+    public EntityV1 apply(GenericRecord command, GenericRecord currentEntityValue) {
 
-        logger.info("CommandExecutor.apply: {}", command.getSchema());
+        String commandTypeName = command.getSchema().getFullName();
+        logger.info("CommandExecutor.apply: {}", commandTypeName);
 
-        // @TODO the logic
+        if ( commandTypeName.equals(CreateEntityRequestV1.class.getName()) ) {
+            return createEntity((CreateEntityRequestV1) command, currentEntityValue);
+            // (Note: This cast ^^^^^^^^^^^^^^^^^^^^^^^  is 'legal' because it is also used in
+            //   https://github.com/confluentinc/schema-registry/blob/master/avro-serde/src/main/java/io/confluent/kafka/streams/serdes/avro/SpecificAvroDeserializer.java )
+            // )
 
-        // @TODO start with unit tests
+        } else if ( commandTypeName.equals(DeleteEntityRequestV1.class.getName()) ) {
+            return deleteEntity((DeleteEntityRequestV1) command, currentEntityValue);
 
-        return newEntityValue;
+        } else {
+            logger.info("CommandExecutor.apply: Uknown request. Command ignored.");
+            return UKNOWN_ENTITY_TO_BE_IGNORED;
+        }
+    }
+
+    private EntityV1 createEntity(CreateEntityRequestV1 command, GenericRecord currentEntityValue) {
+        if (currentEntityValue != null) {
+
+            // We do not change the already existing entity.  (btw: there is PutEntityRequest for this)
+            logger.info("CommandExecutor.apply: Already existing entity. Command ignored.");
+            return ALREADY_EXISTING_ENTITY_TO_BE_IGNORED;
+
+        } else {
+            // We need to create a new entity.
+            logger.info("CommandExecutor.apply: Entity created.");
+            return createNewFromRequest(command);
+        }
+    }
+
+    private EntityV1 deleteEntity(DeleteEntityRequestV1 command, GenericRecord currentEntityValue) {
+        if (currentEntityValue != null) {
+
+            // We delete the existing entity. (i.e. we return null as a tombstone)
+            logger.info("CommandExecutor.apply: Entity deleted.");
+            return null;
+
+        } else {
+            // We do not do anything for the already not existing entity.
+            logger.info("CommandExecutor.apply: Not existing entity. Command ignored.");
+            return NOT_EXISTING_ENTITY_TO_BE_IGNORED;
+        }
+    }
+
+    private EntityV1 createNewFromRequest(CreateEntityRequestV1 createEntityRequest) {
+
+        return EntityV1.newBuilder()
+                .setUuid(UUID.randomUUID().toString())
+                .setEntryDate(System.currentTimeMillis())
+                .setEntityTypeName( createEntityRequest.getEntityTypeName() )
+                .setEntitySubdomainName( createEntityRequest.getEntitySubdomainName() )
+                .setEntityIdInSubdomain( createEntityRequest.getEntityIdInSubdomain() )
+                .setEntityAttributes( createEntityRequest.getEntityAttributes() )
+                .build();
     }
 }
