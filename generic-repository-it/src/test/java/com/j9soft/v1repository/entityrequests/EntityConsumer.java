73c785bc7ff8f874a38b2a59bfd66762618b3757
Jacek Slaby <jacek.slaby@interia.pl>
2018-12-17 11:07:30 +0000

    Added support for tombstones and updates of an entity. (i.e. cases when topic contains many messages with the same key)

--- generic-repository-it/src/test/java/com/j9soft/v1repository/entityrequests/EntityConsumer.java
+++ generic-repository-it/src/test/java/com/j9soft/v1repository/entityrequests/EntityConsumer.java
@@ -9,7 +9,9 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EntityConsumer {
     private static final Logger logger = LoggerFactory.getLogger(RequestProducer.class);
@@ -46,19 +48,47 @@ public class EntityConsumer {
         return result;
     }
 
+    /**
+     * Retrieves the existing entities.
+     * Keeps their order from the topic. Supports tombstones (i.e. delete) and updates.
+     */
     public List<EntityV1> pollAllExistingEntities() {
 
         this.consumer.seekToBeginning( this.consumer.assignment() );
 
-        List<EntityV1> result = new ArrayList<>();
+        List<EntityV1> resultList = new ArrayList<>();
+        // @TODO  we cannot use String as a key
+        //Map<EntityKeyV1, EntityV1> positions = new HashMap<>();
+        Map<String, Integer> positions = new HashMap<>();
+
 
         ConsumerRecords<String, EntityV1> records = consumer.poll(Duration.ofSeconds(2));
         for (ConsumerRecord<String, EntityV1> record : records) {
             String key = record.key();
             EntityV1 value = record.value();
-            result.add(value);
+
+            Integer positionOfPreviousEntryWithTheSameEntity = positions.get(key);
+            if ( positionOfPreviousEntryWithTheSameEntity != null ) {
+                // Set this to be ignored when building result.
+                resultList.set(positionOfPreviousEntryWithTheSameEntity, null);
+                if (value == null) {
+                    // If a tombstone is received it means that this entity was removed
+                    positions.remove(key);
+                }
+            }
+
+            if (value != null) {
+                positions.put(key, resultList.size());
+                resultList.add(value);
+            }
         }
 
+        List<EntityV1> result = new ArrayList<>();
+        for (EntityV1 entity: resultList) {
+            if (entity != null) {
+                result.add(entity);
+            }
+        }
         return result;
     }
 }
